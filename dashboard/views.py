e1a04903c857897f1067eb908b2018a9ad2323ef
diego <diegodatadev@gmail.com>
2025-03-22 05:44:59 -0300

    exemplo iniciais/dummy de templates

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(
-        '<h1 style="color:red">Hello, world. You\'re at the dashboard index.</h1>'
-    )
-    # ^ Retorna uma resposta HTTP com HTML embutido, estilizando o texto diretamente com CSS inline.
+    return render(request, "index.html")
 
 
 def staff(request):
-    # O Django sempre passa um objeto HttpRequest como primeiro argumento para as views,
-    # mesmo que ele não seja utilizado, garantindo compatibilidade com o framework.
-    return HttpResponse("Hello, world. You're at the dashboard staff.")
-
-
-# def index(request):
-#     return render(request, 'dashboard/index.html')
+    return render(request, "staff.html")
